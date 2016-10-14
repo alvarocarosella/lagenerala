a76f5a633b4234a5febb3273afc6eae5361aeabe
Kleer LA <kleer@kleer-sandbox.(none)>
2016-10-14 09:56:12 -0300

    agrege dado.rb

---
+++ lib/dado.rb
@@ -0,0 +1,24 @@
+
+class Dado
+	def initialize 
+		@valor = 0
+	end
+
+	def valor
+		@valor
+	end
+
+	def ponerValor numeroFijo
+		@valor = numeroFijo
+	end
+
+	def lanzarDado
+		if @valor == 0
+			numero = rand(6)+1
+		else
+			numero = @valor
+			@valor = 0
+		end
+		return numero
+	end
+end
